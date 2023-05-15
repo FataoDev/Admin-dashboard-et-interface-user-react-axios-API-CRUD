0990800383762c1b716a52add05140d49f3786db
Fatao OUEDRAOGO <fataoouedraogo226@gmail.com>
2023-05-15 10:29:58 +0000

    Design de la page 404

--- src/_helpers/AuthGuard.js
+++ src/_helpers/AuthGuard.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { accountService } from "@/_services/account_service";
+import Error from "@/_utils/Error";
 
 const AuthGuard = ({ children }) => {
   if (!accountService.isLogged()) {
     return <Navigate to="/auth/login" />;
   } else {
     const user = accountService.getTokenInfo();
-    console.log(user);
     if (user.email === "fataoouedraogo226@gmail.com") {
       return children;
+    } else {
+      return <Error />;
     }
   }
   // return children;
--- src/_utils/Error.js
+++ src/_utils/Error.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./error.css";
 
 const Error = () => {
-    return (
-        <div>
-            Ereur 404 !
-        </div>
-    );
+  return (
+    <div className="error">
+      <div>Erreur 404 !</div>
+      <div>Page Not Found</div>
+      <Link to="/">
+        <button className="btn">Go to Home</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Error;
