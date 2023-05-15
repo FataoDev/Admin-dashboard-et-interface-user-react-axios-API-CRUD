654c5d49bb10993229a2382ba8d0fe5e4769bd81
Fatao OUEDRAOGO <fataoouedraogo226@gmail.com>
2023-05-15 09:57:02 +0000

    limitation de l'accès à l'interface administrateur

--- src/_helpers/AuthGuard.js
+++ src/_helpers/AuthGuard.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { accountService } from '@/_services/account_service';
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { accountService } from "@/_services/account_service";
 
-const AuthGuard = ({children}) => {
-    if(!accountService.isLogged()) return <Navigate to="/auth/login"/>
-    return children
+const AuthGuard = ({ children }) => {
+  if (!accountService.isLogged()) {
+    return <Navigate to="/auth/login" />;
+  } else {
+    const user = accountService.getTokenInfo();
+    console.log(user);
+    if (user.email === "fataoouedraogo226@gmail.com") {
+      return children;
+    }
+  }
+  // return children;
 };
 
-export default AuthGuard; 
+export default AuthGuard;
--- src/components/public/Header.js
+++ src/components/public/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./header.css";
 import { accountService } from "@/_services/account_service";
