262a01e44404b0bb8d7eaaf59c082102f59df864
Fatao OUEDRAOGO <fataoouedraogo226@gmail.com>
2023-05-30 09:49:35 +0000

    ajustement de la taille des produits à l'accueil

--- src/components/admin/Header.js
+++ src/components/admin/Header.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import {accountService} from '@/_services/account_service';
-import '@/pages/Admin/admin.css'
+import React from "react";
+// import { useNavigate } from "react-router-dom";
+// import { accountService } from "@/_services/account_service";
+import "@/pages/Admin/admin.css";
 
 const Header = () => {
-    let navigate= useNavigate()
-    const logout=()=>{
-        accountService.logout()
-        navigate('/')
-    }
+  //   let navigate = useNavigate();
+  // const logout=()=>{
+  //     accountService.logout()
+  //     navigate('/')
+  // }
 
-    return (
-        <div className='AHeader'>
-            Interface administrateur
-            <button onClick={logout} id='btn-header'>Déconnecter</button>
-        </div>
-    );
+  return (
+    <div className="AHeader">
+      Interface administrateur
+      {/*    <button onClick={logout} id='btn-header'>Déconnecter</button> */}
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/Admin/ALayout.js
+++ src/pages/Admin/ALayout.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-// import Header from '@/components/admin/Header'
+import Header from '@/components/admin/Header'
 import SideMenu from "../../components/admin/SideMenu";
 import "./admin.css";
 
 const ALayout = () => {
   return (
     <div className="ALayout">
-      {/* <Header/> */}
+      <Header/>
       <div id="admin">
         <SideMenu />
         <div id="admin_body">
